69e45395ee7ef6fe4e1e990d2c939e76c0e425d3
mailsky2010 <mailsky2010@gmail.com>
2023-02-28 10:18:05 +0900

    Changes

--- src/main/java/org/example/intel/Application.java
+++ src/main/java/org/example/intel/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        System.out.println("main() >>>>>>>>>>>>>>>>>>>>>>>>>" + args);
+        System.out.println("main() args >>>>>>>>>>>>>>>>>>>>>>>>>" + args);
         SpringApplication.run(Application.class, args);
     }
 }
